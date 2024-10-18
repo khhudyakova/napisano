7c0bf6d486f0a889c0b17914aabaa2401b590060
rootah <113906844+khhudyakova@users.noreply.github.com>
2024-10-18 17:26:11 +0300

    Add files via upload

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+    def index
+    end
+  
+    def news
+    end
+  
+    def shows
+    end
+  
+    def authors
+    end
+  
+    def popular
+    end
+  
+    def account
+    end
+  end
+  
+  
